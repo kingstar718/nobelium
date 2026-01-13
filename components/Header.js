e881b0a01b7cde0de681646b412c927edd04427e
Richard Wu <36195120+kingstar718@users.noreply.github.com>
2026-01-13 16:11:08 +0800

    Update Header.js 修改哨兵栏、导航栏间距

--- components/Header.js
+++ components/Header.js
@@ -83,9 +83,9 @@ export default function Header ({ navBarTitle, fullWidth }) {
 
   return (
     <>
-      <div className="observer-element h-4 md:h-12" ref={sentinelRef}></div>
+      <div className="observer-element h-4 md:h-4" ref={sentinelRef}></div>
       <div
-        className={`sticky-nav group m-auto w-full h-6 flex flex-row justify-between items-center mb-2 md:mb-12 py-8 bg-opacity-60 ${
+        className={`sticky-nav group m-auto w-full h-6 flex flex-row justify-between items-center mb-2 md:mb-6 py-6 bg-opacity-60 ${
           !fullWidth ? 'max-w-3xl px-4' : 'px-4 md:px-24'
         }`}
         id="sticky-nav"
